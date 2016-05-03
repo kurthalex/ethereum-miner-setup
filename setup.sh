2860fd2dc208156b5ea4ec2967470e7a6e1ba771
Alexandre Kurth <alexandre.kurth@gmail.com>
2016-05-04 01:54:10 +0200

    update setup for single chunk failed

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install vim wget curl git terminator htop xorg xfce4 xdm fglrx-updates
+sudo apt-get -y install vim wget htop fglrx-updates
 wget wget http://ethpool.org/downloads/qtminer_1504.tgz
 tar zxvf qtminer_1504.tgz
 sudo chmod +x qtminer.sh
-sudo reboot
+cat >> .bashrc << EOF
+export GPU_SINGLE_ALLOC_PERCENT=100
+export GPU_MAX_ALLOC_PERCENT=100
+export GPU_MAX_HEAP_SIZE=100
+export GPU_USE_SYNC_OBJECTS=1
+EOF
+sudo shutdown now
