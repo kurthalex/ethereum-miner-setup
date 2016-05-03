c340226b5496d1edefc6cc515ef72f9cd8411d24
Alexandre Kurth <alexandre.kurth@gmail.com>
2016-05-03 23:31:05 +0200

    update setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get install vim wget curl git terminator htop xorg lxde fglrx-updates
+sudo apt-get -y install vim wget curl git terminator htop xorg lxde fglrx-updates
 wget wget http://ethpool.org/downloads/qtminer_1504.tgz
 tar zxvf qtminer_1504.tgz
 sudo chmod +x qtminer.sh
-
+sudo shutdown now
